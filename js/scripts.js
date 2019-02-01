63004f812edfd15ce503008f5790164aedbdffc4
lm <leo.mangutov@gmail.com>
2019-02-01 15:57:48 -0800

    add constructors and methods to pizza orders

--- js/scripts.js
+++ js/scripts.js
@@ -38,56 +38,55 @@ PizzaOrder.prototype.findOrder = function(id) {
 }
 
 function Pizza(size, crust, type, item) {
-  this.size = size
+  this.size = size,
   this.crust = crust,
-  this.type = type
-  this.item = item
-  this.price = 8
+  this.type = type,
+  this.item = item,
+  this.price = 14
 }
 
 Pizza.prototype.fullDestination = function() {
   return this.date + " " + this.country + " " + this.city;
 }
 
-var logBook = new LogBook();
+var pizzaOrder = new PizzaOrder();
 
-function displayEntryDetails(logBookToDisplay) {
-  var entryList = $("ul#places");
-  var htmlForEntryInfo = "";
-  logBookToDisplay.entries.forEach(function(place) {
-    htmlForEntryInfo += "<li id=" + place.id + ">" + place.country + " and " + place.city + "</li>";
+function displayOrderDetails(orderToDisplay) {
+  var orderList = $("ul#orders");
+  var htmlForOrderInfo = "";
+  orderToDisplay.orders.forEach(function(order) {
+    htmlForOrderInfo += "<li id=" + order.id + ">" + order.size + " and " + order.crust + "</li>";
   });
-  entryList.html(htmlForEntryInfo)
+  orderList.html(htmlForOrderInfo)
 };
 
-function attachEntryListeners() {
-  $("ul#places").on("click", "li", function() {
-    showEntries(this.id);
+function attachOrderListeners() {
+  $("ul#orders").on("click", "li", function() {
+    showOrders(this.id);
   });
   $("#buttons").on("click", ".deleteButton", function() {
-    logBook.deleteEntry(this.id);
-    $("#show-places").hide();
-    displayEntryDetails(logBook);
+    orders.deleteOrder(this.id);
+    $("#show-orders").hide();
+    displayOrderDetails(orders);
   });
 };
 
-function showEntries(entriesId) {
-  var entry = logBook.findEntry(entriesId);
-  $("show-places").show();
-  $(".city-destination").html(entry.city);
-  $(".country-destination").html(entry.country);
-  $(".date-travel").html(entry.date);
-  $(".duration-trip").html(entry.duration);
-  $(".landmark").html(entry.landmark);
-  $(".notes").html(entry.note);
+function showOrders(ordersId) {
+  var order = pizzaOrder.findOrder(ordersId);
+  $("show-orders").show();
+  $(".pizza-size").html(order.size);
+  $(".pizza-crust").html(order.crust);
+  $(".pizza-type").html(order.type);
+  $(".item").html(order.item);
+  $(".total").html(order.total);
   var buttons = $("#buttons");
   buttons.empty();
-  buttons.append("<button class='deleteButton' id=" + entry.id + ">Delete</button>")
+  buttons.append("<button class='deleteButton' id=" + order.id + ">Delete Order</button>")
 };
 
 $(document).ready(function() {
-  attachEntryListeners();
-  $("form#new-log").submit(function(event) {
+  attachOrderListeners();
+  $("form#new-pizza").submit(function(event) {
     event.preventDefault();
     var inputtedCity = $("#new-city-destination").val();
     var inputtedCountry = $("#new-country-destination").val();
@@ -96,17 +95,10 @@ $(document).ready(function() {
     var inputtedLandmark = $("#new-landmark").val();
     var inputtedNote = $("#new-notes").val();
 
-    $("#new-city-destination").val("");
-    $("#new-country-destination").val("");
-    $("#new-date-travel").val("");
-    $("#new-duration-trip").val("");
-    $("#new-landmark").val("");
-    $("#new-notes").val("");
-
-    var newEntry = new Pizza(inputtedCity, inputtedCountry, inputtedDate, inputtedDuration, inputtedLandmark, inputtedNote);
-    logBook.addEntry(newEntry);
-    displayEntryDetails(logBook);
-    console.log(newEntry);
+    var newOrder = new Pizza(inputtedCity, inputtedCountry, inputtedDate, inputtedDuration, inputtedLandmark, inputtedNote);
+    pizzaOrder.addOrder(newOrder);
+    displayOrderDetails(pizzaOrder);
+    console.log(newOrder);
 
   });
 });
